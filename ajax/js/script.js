dfd9d7c6c25bb5353c813e4d4b9425922ac0f7b6
Aniela Fendrych <aniela.fendrych@gmail.com>
2023-09-04 13:02:53 +0200

    $.getJSON()-methods

--- ajax/js/script.js
+++ ajax/js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-    // dwie metody uzycia funkcji 'get' w jQuery
-    //zrodlo, funkcja; przy metodzie get zamiana stringa na obiekt JavaScript
-    $.get('https://jsonplaceholder.typicode.com/users/1', function(data) {
+    // dwie metody uzycia funkcji 'getJSON' w jQuery
+    //pobieranie jedynie obiektów typu JSON
+    $.getJSON('https://jsonplaceholder.typicode.com/users/1', function(data) {
         console.log(data);
         console.log(data.name);
         console.log(data.email);
     });
 
-    $.get('https://jsonplaceholder.typicode.com/users/1')
+    $.getJSON('https://jsonplaceholder.typicode.com/users/1')
         .done(function(data) {
             console.log(data);
             console.log(data.name);
